607406b626b28212280aaf23cb0d7526b357b84e
Rafael Soares <sawarafael@gmail.com>
2021-07-30 16:09:15 -0300

    Fix: Rota de Atualização de Dados

---
+++ API/controllers/updates/updSector.js
@@ -0,0 +1,45 @@
+const collabUser = require('../../models/collaborators/collabUser');
+const collabData = require('../../models/collaborators/collabData');
+
+module.exports = {
+    updateDepartament (req, res) {
+        collabUser.findOne({
+            where: {
+                id: req.query.id
+            },
+            attributes: ['id']
+        }).then(collab => {
+            if (!collab) {
+                res.status(404).json({ err: "Colaborador não existe!" })
+            }
+
+            if (req.body.group) {
+                collabData.update({
+                    teamId   :   req.body.group,
+                    status   :   req.body.status
+                }, {
+                    where: {
+                        id: collab.id
+                    }
+                }).catch(err => {
+                    res.status(400).json({ err: "Erro em atualizar o Grupo do qual o Colaborador pertence." })
+                })
+                res.status(200).json({ msg: "Sucesso em Atualizar o Grupo do Colaborador." })
+            } else if (req.body.departament) {
+                collabData.update({
+                    departamentId   :   req.body.departament,
+                    status          :   req.body.status
+                }, {
+                    where: {
+                        id: collab.id
+                    }
+                }).catch(err => {
+                    res.status(400).json({ err: "Erro em atualizar o Departamento do qual o Colaborador pertence." })
+                })
+                res.status(200).json({ msg: "Sucesso em Atualizar o Departamento do Colaborador." })
+            }
+        }).catch(err => {
+            res.status(401).json({ err: "Colaborador não encontrado." })
+        })
+    }
+}
--- API/routes/UpdtRoute.js
+++ API/routes/UpdtRoute.js
@@ -2,8 +2,11 @@ const express = require('express');
 const UpdateRoute = express.Router();
 
 const addDataController = require('./../controllers/updates/addData');
+const updSectorController = require('./../controllers/updates/updSector');
 
 UpdateRoute.post('/add-data', addDataController.addData);
 UpdateRoute.post('/add-file', addDataController.addFile);
+
+UpdateRoute.patch('/upd-sector', updSectorController.updateDepartament);
 
 module.exports = UpdateRoute;
