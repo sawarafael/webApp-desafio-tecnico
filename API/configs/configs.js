afbe341afe905e301f563a8dd908c87d9057125d
Rafael Soares <sawarafael@gmail.com>
2021-07-30 19:48:31 -0300

    Fix: configs

--- API/configs/configs.js
+++ API/configs/configs.js
@@ -2,7 +2,7 @@ module.exports = {
     PORT: 3000,
 
     DB_NAME : "collabsDB",
-    DB_USER : "root",
-    DB_PASS : "Sawada01",
+    DB_USER : "         ",
+    DB_PASS : "         ",
     DB_HOST : "localhost"
 }
