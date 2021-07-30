19576de24446f50faa4eb6c39a248dc2d21608f4
Rafael Soares <sawarafael@gmail.com>
2021-07-30 15:21:26 -0300

    Fix: Rota de Atualização de Dados

--- API/controllers/updates/addData.js
+++ API/controllers/updates/addData.js
@@ -37,5 +37,37 @@ module.exports = {
         }).catch(err => {
             res.status(401).json({ err: "Colaborador não encontrado." })
         })
+    },
+
+    addFile (req, res) {
+        collabUser.findOne({
+            where: {
+                id: req.query.id
+            },
+            attributes: ['id']
+        }).then(collab => {
+            if (!collab) {
+                res.status(404).json({ err: "Colaborador não existe!" })
+            }
+
+            const newFile = {
+                name    : req.body.name,
+                newFile : req.body.file,
+            }
+
+            collabFile.update({
+                name    : newFile.name,
+                file    : newFile.file
+            }, {
+                where: {
+                    id: collab.id
+                }
+            }).catch(err => {
+                res.status(400).json({ err: "Erro em atualizar o arquivo do Colaborador." })
+            })
+            res.status(200).json({ msg: "Arquivo do Colaborador Alterado com Sucesso" })
+        }).catch(err => {
+            res.status(401).json({ err: "Colaborador não encontrado." })
+        })
     }
 }
--- API/routes/UpdtRoute.js
+++ API/routes/UpdtRoute.js
@@ -4,5 +4,6 @@ const UpdateRoute = express.Router();
 const addDataController = require('./../controllers/updates/addData');
 
 UpdateRoute.post('/add-data', addDataController.addData);
+UpdateRoute.post('/add-file', addDataController.addFile);
 
 module.exports = UpdateRoute;
