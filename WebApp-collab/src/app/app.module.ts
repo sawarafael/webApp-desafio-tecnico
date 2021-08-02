b3a4b35cee3d0b8653e46959e4ea714913d13931
Rafael Soares <sawarafael@gmail.com>
2021-08-02 18:23:21 -0300

    Features: pages/login~signup~records

--- WebApp-collab/src/app/app.module.ts
+++ WebApp-collab/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { RecordsComponent } from './pages/records/records.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    RecordsComponent
   ],
   imports: [
     BrowserModule,
