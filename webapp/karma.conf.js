2c09192379c8a7bf3780767ef6ca03090b766bf0
Rafael Soares <sawarafael@gmail.com>
2021-08-03 17:31:51 -0300

    Fix: Front-End

--- webapp/karma.conf.js
+++ webapp/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/WebApp-collab'),
+      dir: require('path').join(__dirname, './coverage/webapp'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -3,16 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
-import { SignupComponent } from './pages/signup/signup.component';
-import { RecordsComponent } from './pages/records/records.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    SignupComponent,
-    RecordsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
